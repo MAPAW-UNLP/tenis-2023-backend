d2b4d28084629d57d3447ede845ae61ce5cb79c6
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2023-12-11 00:09:10 -0300

    Agrega filtro pagos profesor

--- src/src/Repository/PagosRepository.php
+++ src/src/Repository/PagosRepository.php
@@ -41,53 +41,64 @@ class PagosRepository extends ServiceEntityRepository
 
 
     /**
-    * @return Pagos[] Returns an array of Pagos objects
-    */
-   public function findPagosByPersonaId($personaId): array
-   {
-       return $this->createQueryBuilder('p')
-           ->andWhere('p.idPersona = :val')
-           ->setParameter('val', $personaId)
-           ->orderBy('p.id', 'ASC')
-           ->getQuery()
-           ->getResult()
-       ;
-   }
+     * @return Pagos[] Returns an array of Pagos objects
+     */
+    public function findPagosByPersonaId($personaId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.idPersona = :val')
+            ->setParameter('val', $personaId)
+            ->orderBy('p.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 
-        /**
-        * @return Pagos[] Returns an array of Pagos objects
-        */
-        public function findAll(): array
-        {
-            return $this->createQueryBuilder('c')
-                ->getQuery()
-                ->getResult()
-            ;
-        }
+    /**
+     * @return Pagos[] Returns an array of Pagos objects
+     */
+    public function findAll(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findPagosByProfesorIdInDates($profesorId, $primerDia, $ultimoDia): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.profesor = :profesorId')
+            ->andWhere('p.fecha >= :primerDia') // Para obtener solo pagos futuros
+            ->andWhere('p.fecha <= :ultimoDia') // Para obtener solo pagos futuros
+            ->setParameter('profesorId', $profesorId)
+            ->setParameter('primerDia', $primerDia) // Corregido aquí
+            ->setParameter('ultimoDia', $ultimoDia) // Corregido aquí
+            ->getQuery()
+            ->getResult();
+    }
 
 
-//    /**
-//     * @return Pagos[] Returns an array of Pagos objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Pagos[] Returns an array of Pagos objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('p.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Pagos
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Pagos
+    //    {
+    //        return $this->createQueryBuilder('p')
+    //            ->andWhere('p.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
